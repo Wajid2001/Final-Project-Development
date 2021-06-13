9c429f8632548c9d2fc3033c25a5a4c1372c7c46
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-13 16:46:52 +0530

    Revert "Update tailwind.config.js"
    This reverts commit 4575602437f001d88b75eb860bb7bb6b75d70cd5.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-	purge: ["./React_Files/**/*.jsx"],
+	// This is for Production
+	// purge: {
+	// 	enabled: true,
+	// 	content: ["./React_Files/**/*.jsx"],
+	// },
+
+	// This is for Development
+	purge: [],
+
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
