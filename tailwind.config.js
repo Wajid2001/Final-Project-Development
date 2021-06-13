0ac0dd6adaad93b7e1d03ace7d705fa57db43131
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-13 11:36:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-	purge: {
-		enabled: true,
-		content: ["./React_Files/**/*.jsx"],
-	},
+	// This is for Production
+	// purge: {
+	// 	enabled: true,
+	// 	content: ["./React_Files/**/*.jsx"],
+	// },
+
+	// This is for Development
+	purge: [],
+
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
